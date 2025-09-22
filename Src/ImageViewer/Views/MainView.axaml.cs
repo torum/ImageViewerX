0d8e01182713715cf1e91c9bddee7eca62634c25
torum <37834607+torum@users.noreply.github.com>
2025-09-22 15:07:21 +0900

    init

---
+++ Src/ImageViewer/Views/MainView.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.PanAndZoom;
+using Avalonia.Input;
+using System.Diagnostics;
+
+namespace ImageViewer.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+
+    }
+
+}
