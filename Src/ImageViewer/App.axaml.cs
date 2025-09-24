ba0fbb93c8c423ec7c64de4202c71fa7ee344b9d
torum <37834607+torum@users.noreply.github.com>
2025-09-25 08:50:05 +0900

    more experiments

--- Src/ImageViewer/App.axaml.cs
+++ Src/ImageViewer/App.axaml.cs
@@ -5,11 +5,40 @@ using Avalonia.Markup.Xaml;
 
 using ImageViewer.ViewModels;
 using ImageViewer.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ImageViewer;
 
 public partial class App : Application
 {
+    public IHost AppHost { get; private set; }
+
+    public App()
+    {
+        AppHost = Microsoft.Extensions.Hosting.Host
+                .CreateDefaultBuilder()
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                    services.AddSingleton<MainView>();
+                    services.AddSingleton<MainViewModel>();
+                })
+                .Build();
+    }
+
+    public static T GetService<T>() where T : class
+    {
+        if ((App.Current as App)!.AppHost!.Services.GetService(typeof(T)) is not T service)
+        {
+            throw new ArgumentException($"{typeof(T)} needs to be registered in ConfigureServices within App.xaml.cs.");
+        }
+
+        return service;
+    }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -23,10 +52,14 @@ public partial class App : Application
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            /*
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new MainViewModel()
             };
+            */
+
+            desktop.MainWindow = App.GetService<MainWindow>();
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
