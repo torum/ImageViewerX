4b1281f15c6117a0fe5eca393992c41e5257662c
torum <37834607+torum@users.noreply.github.com>
2025-09-29 01:21:27 +0900

    Added ListBox

---
+++ Src/ImageViewer/Models/ImageInfo.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageViewer.Models;
+
+public class ImageInfo : ObservableObject
+{
+    public bool IsLoading { get; set; } = false;
+    
+    public bool IsAcquired { get; set; } = false;
+
+    public string? ImageFilePath { get; set; }
+
+    public int BinarySize { get; set; }
+
+    private Bitmap? _imageSource;
+    public Bitmap? ImageSource
+    {
+        get
+        {
+            return _imageSource;
+        }
+        set
+        {
+            if (_imageSource == value)
+                return;
+
+            _imageSource = value;
+            OnPropertyChanged(nameof(ImageSource));
+        }
+    }
+}
+
+
